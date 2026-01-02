99e28d6b0f6b8440a397a789b625f6951823b00f
YerdosNar <besway2000@gmail.com>
2026-01-02 19:44:41 +0900

    Just refurbished

--- password_generator/generator.asm
+++ password_generator/generator.asm
@@ -5,13 +5,13 @@ section .data
     flag_long   db "--length",  0
 
 section .text
-    global main
-    extern printf
-    extern rand
-    extern srand
-    extern time
-    extern strcmp
-    extern atoi
+    global  main
+    extern  printf
+    extern  rand
+    extern  srand
+    extern  time
+    extern  strcmp
+    extern  atoi
 
 main:
     push    rbp                 ; save base pointer
@@ -53,7 +53,7 @@ start_setup:
     ; loop counter init (i = 0)
     ; using RBX, `callee-saved`
     ; funcs like `printf`, `rand` cannot chage RBX
-    xor     rbx, rbx    ; this is how we get zero
+    xor     rbx, rbx            ; this is how we get zero
 
 ; To prevent
 ; First char != number
@@ -64,72 +64,72 @@ first_iteration:
     jmp     generate_lowercase
 
 loop_start:
-    cmp	    rbx, r12; comapring i to 10
-    jge loop_end    ; jge(jump grater or equal) i>=10
+    cmp	    rbx, r12            ; comapring i to 10
+    jge     loop_end            ; jge(jump grater or equal) i>=10
 
     ; Generate type: 0=lower, 1=upper, 2=generate_number
-    call rand       ; return random int in RAX
-    xor rdx, rdx    ; RDX = 0
-    mov rcx, 3      ; divide to 3
-    div rcx         ; remainder in RDX
+    call    rand                ; return random int in RAX
+    xor     rdx, rdx            ; RDX = 0
+    mov     rcx, 3              ; divide to 3
+    div     rcx                 ; remainder in RDX
 
     ; if 0 -> generate_lowercase
-    cmp rdx, 0      ; compare with 0
-    je generate_lowercase ;
+    cmp     rdx, 0              ; compare with 0
+    je      generate_lowercase  ;
 
     ; if 1 -> generate_uppercase
-    cmp rdx, 1
-    je generate_uppercase ;
+    cmp     rdx, 1
+    je      generate_uppercase  ;
 
     ; else -> generate_number
-    jmp generate_number
+    jmp     generate_number
 
 
 generate_uppercase:
     ; char c = rand() % 26 + 65
-    call rand       ; RAX random number
-    xor rdx, rdx    ; RDX set to zero
-    mov rcx, 26     ; divisor
-    div rcx         ; divide RAX by 26, remainder in RDX
-    add rdx, 65     ; add 65 to go to uppercase letters
-    jmp print_char
+    call    rand                ; RAX random number
+    xor     rdx, rdx            ; RDX set to zero
+    mov     rcx, 26             ; divisor
+    div     rcx                 ; divide RAX by 26, remainder in RDX
+    add     rdx, 65             ; add 65 to go to uppercase letters
+    jmp     print_char
 
 generate_lowercase:
     ; char c = rand() % 26 + 97
-    call rand       ; RAX random number
-    xor rdx, rdx    ; RDX set to zero
-    mov rcx, 26     ; divisor
-    div rcx         ; divide RAX by 26, remainder in RDX
-    add rdx, 97     ; add 97 to go to uppercase letters
-    jmp print_char
+    call    rand                ; RAX random number
+    xor     rdx, rdx            ; RDX set to zero
+    mov     rcx, 26             ; divisor
+    div     rcx                 ; divide RAX by 26, remainder in RDX
+    add     rdx, 97             ; add 97 to go to uppercase letters
+    jmp     print_char
 
 generate_number:
     ; char c = rand() % 10 + 48
-    call rand       ; RAX rand number
-    xor rdx, rdx
-    mov rcx, 10
-    div rcx
-    add rdx, 48
-    jmp print_char
+    call    rand                ; RAX rand number
+    xor     rdx, rdx
+    mov     rcx, 10
+    div     rcx
+    add     rdx, 48
+    jmp     print_char
 
 print_char:
     ; printf("%c", c)
-    mov rdi, fmt_char ; first arg: "%c"
-    mov rsi, rdx      ; second argc: the char `c`
-    xor rax, rax    ; RAX = 0
-    call printf
+    mov     rdi, fmt_char       ; first arg: "%c"
+    mov     rsi, rdx            ; second argc: the char `c`
+    xor     rax, rax            ; RAX = 0
+    call    printf
 
-    inc rbx         ; i++
-    jmp loop_start  ; repeat loop
+    inc     rbx                 ; i++
+    jmp     loop_start          ; repeat loop
 
 loop_end:
     ; printf("\n")
-    mov rdi, fmt_char
-    mov rsi, 10
-    xor rax, rax
-    call printf
+    mov     rdi, fmt_char
+    mov     rsi, 10
+    xor     rax, rax
+    call    printf
 
     ; return 0
-    xor rax, rax
-    pop rbp
+    xor     rax, rax
+    pop     rbp
     ret
--- password_generator/run.sh
+++ password_generator/run.sh
@@ -4,12 +4,12 @@ if [ "$1" == "asm" ];then
     nasm generator.asm -f elf64 -o generator.o
     gcc -o generator generator.o -no-pie
     read -p "Would you to give flags: " flag
-    if [ ! -z $flag ]; then
-        read -p "Flag '$flag' received: give following: " length
-        ./generator "$flag" "$length"
-    else
+    if [ -z $flag ]; then
         echo "Nothing is received: Running noflags"
         ./generator
+    else
+        read -p "Flag '$flag' received: give following: " length
+        ./generator "$flag" "$length"
     fi
     rm generator generator.o
 else
