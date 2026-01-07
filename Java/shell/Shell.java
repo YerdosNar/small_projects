1aa548c970e3a3937b5dfc353a8fdf386aa4544e
YerdosNar <besway2000@gmail.com>
2026-01-07 23:01:51 +0900

    username, homedir, currDir are done

--- Java/shell/Shell.java
+++ Java/shell/Shell.java
@@ -1,9 +1,20 @@
-// import java.util.*;
+import java.util.Scanner;
 
 public class Shell {
     public static void main(String[] args) {
-        System.out.println("PWD: = " + System.getProperty("user.dir"));
-        String pwd = System.getProperty("user.dir");
-        System.out.println(pwd);
+        Scanner sc = new Scanner(System.in);
+        String homeDir = System.getProperty("user.home");
+        String rawPath = System.getProperty("user.dir");
+        String path = rawPath.replace(homeDir, "~");
+        String userName = System.getProperty("user.name");
+        System.out.print(userName + path + "$ ");
+
+        String rawLine = sc.nextLine();
+        String[] tokens = rawLine.split(" ");
+        for(int i = 0; i < tokens.length; i++) {
+            System.out.println(tokens[i]);
+        }
+
+        sc.close();
     }
 }
--- Java/shell/run.sh
+++ Java/shell/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -f Shell.class || Shell.java -nt Shell.class ]]; then
+    echo "Compiling..."
     javac Shell.java
 fi
 
