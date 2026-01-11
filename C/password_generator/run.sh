7854bf46b268d471cb9dc6fdc4a2ba55507548ce
YerdosNar <besway2000@gmail.com>
2026-01-12 03:53:20 +0900

    Updated 'run.sh'

--- C/password_generator/run.sh
+++ C/password_generator/run.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "asm" ];then
+    shift 1
     nasm generator.asm -f elf64 -o generator.o
     gcc -o generator generator.o -no-pie
-    read -p "Would you to give flags: " flag
-    if [ -z $flag ]; then
-        echo "Nothing is received: Running noflags"
-        ./generator
-    else
-        read -p "Flag '$flag' received: give following: " length
-        ./generator "$flag" "$length"
-    fi
+    echo "$@"
+    ./generator "$@"
+
     rm generator generator.o
 else
     gcc -o generator generator.c -Wall -Wextra
