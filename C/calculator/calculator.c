b51087a13bdc7b0791b3b6b8983e691d54926992
YerdosNar <besway2000@gmail.com>
2026-01-28 15:43:45 +0900

    Updating calculator

--- C/calculator/calculator.c
+++ C/calculator/calculator.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    printf("Enter a: ");
-    scanf("%d", &a);
-    printf("Enter b: ");
-    scanf("%d", &b);
-    printf("Result: %d\n", a+b);
+    char expression[128];
+    printf("Enter an expression: ");
+    fgets(expression, 127, stdin);
+    printf("Your expression: %s\n", expression);
 
     return 0;
 }
