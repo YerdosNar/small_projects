7376f86df449ba0bca9f5d6f94ba3fec3029b2aa
YerdosNar <besway2000@gmail.com>
2026-01-29 21:34:34 +0900

    Working exemplar

--- C/calculator/calculator.c
+++ C/calculator/calculator.c
@@ -1,47 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int find_sign(char *string, char sign) {
-    for(int i = 0; i < strlen(string); i++) {
-        if (string[i] == sign) {
-            return i;
-        }
+float evaluate(char *num1, char operand, char *num2) {
+    if(operand == '*') {
+        return atof(num1) * atof(num2);
     }
-    return -1;
+    else if(operand == '/') {
+        return atof(num1) / atof(num2);
+    }
+    else if(operand == '+') {
+        return atof(num1) + atof(num2);
+    }
+    else {
+        return atof(num1) - atof(num2);
+    }
 }
-
-// void get_substring(char *string, char *sub_string, int from, int to) {
-//     int i = 0;
-//     while(from+i < to) {
-//         sub_string[i] = string[from+i];
-//         printf("from get_substring: %s\n", sub_string);
-//         i++;
-//     }
-// }
-// void tokenize(char *str, char **tokens) {
-//     int start = 0, end = 0, idx = 0;
-//     for(int i = 0; i < strlen(str); i++) {
-//         if(str[i] == ' '
-//         || str[i] == '*'
-//         || str[i] == '/'
-//         || str[i] == '+'
-//         || str[i] == '-'
-//         || str[i] == '%'
-//         || str[i] == ')'
-//         || str[i] == '(') {
-//             get_substring(str, tokens[idx++], start, i);
-//             start = i;
-//         }
-//         i++;
-//     }
-// }
 
 int main() {
     char expression[128];
     printf("Enter an expression: ");
     fgets(expression, 127, stdin);
 
-    printf("%d\n", find_sign(expression, '*'));
     char *tokens[64];
     int i = 0;
     char *token = strtok(expression, " ");
@@ -50,11 +30,19 @@ int main() {
         i++;
         token = strtok(NULL, " ");
     }
-    printf("Your expression: \n");
-    for(int j = 0; j < 64; j++) {
-        printf("%s\n", tokens[j]);
+
+    float result;
+    for (int j = 0; j < i; j++) {
+        if(!strncmp(tokens[j], "*", 1)
+        || !strncmp(tokens[j], "/", 1)
+        || !strncmp(tokens[j], "+", 1)
+        || !strncmp(tokens[j], "-", 1))
+        {
+            result = evaluate(tokens[j-1], tokens[j][0], tokens[j+1]);
+        }
     }
 
+    printf("result: %.2f\n", result);
 
     return 0;
 }
