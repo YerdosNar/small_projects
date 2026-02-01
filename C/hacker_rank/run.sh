8521c9742479ae7547b8c9c6bb9f8f9b1eb01250
YerdosNar <besway2000@gmail.com>
2026-02-02 04:53:47 +0900

    updated run.sh script

--- C/hacker_rank/run.sh
+++ C/hacker_rank/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]; then
+    echo "Enter the filename to compile"
+    exit
+fi
+
 filename=$1
 exe_name=${filename%.*}
 gcc -o $exe_name $filename -Wall -Wextra -lm
