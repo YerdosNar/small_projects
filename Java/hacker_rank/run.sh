71519162842ec82909512066f0542b2d8e4db645
YerdosNar <besway2000@gmail.com>
2026-02-03 01:24:25 +0900

    updated cleaning option

--- Java/hacker_rank/run.sh
+++ Java/hacker_rank/run.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 0 ]; then
-    echo "+----------------------------+"
-    echo "| Usage: ./run.sh <filename> |"
-    echo "+----------------------------+"
-    echo "| Example:                   |"
-    echo "|          ./run.sh main.c   |"
-    echo "+----------------------------+"
+    echo "+--------------------------------------+"
+    echo "| Usage: ./run.sh <filename>           |"
+    echo "+--------------------------------------+"
+    echo "| Options:                             |"
+    echo "|     -c/--clean Clean all .class files|"
+    echo "+--------------------------------------+"
+    echo "| Example:                             |"
+    echo "|          ./run.sh main.c             |"
+    echo "+--------------------------------------+"
     exit
 elif [[ "$1" == "-c" || "$1" == "--clean" ]]; then
+    for file in *.class; do
+        if [ -f $file ]; then
+            echo "Found: $file"
+        else
+            echo "Nothing found..."
+            exit
+        fi
+    done
     echo -e "\033[1;31mCLEANING...\033[0m"
     rm *.class
+    exit
 fi
 
 filename=$1
