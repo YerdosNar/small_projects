72d3d1b148b3b3198683a6fa1c84878246b03433
YerdosNar <besway2000@gmail.com>
2026-03-02 11:10:58 +0900

    hacker rank software engineer prep kit
    Added new solutions
    Updated 'run' script

--- C/hacker_rank/run.sh
+++ C/hacker_rank/run.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then
-    echo "+----------------------------+"
-    echo "| Usage: ./run.sh <filename> |"
-    echo "+----------------------------+"
-    echo "| Example:                   |"
-    echo "|          ./run.sh main.c   |"
-    echo "+----------------------------+"
+if [[ $# -eq 0 || "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "+-----------------------------------------+"
+    echo "| Usage: ./run.sh [opts] <filename>       |"
+    echo "+-----------------------------------------+"
+    echo "| Options:                                |"
+    echo "|     -h, --help      Help message        |"
+    echo "|     -c, --clean     Clean up            |"
+    echo "|     -o, --only <f>  Only compile        |"
+    echo "|     [filename]      Compile and execute |"
+    echo "+-----------------------------------------+"
+    echo "| Example:                                |"
+    echo "|          ./run.sh main.c                |"
+    echo "+-----------------------------------------+"
     exit
 fi
 
-filename=$1
-exe_name=${filename%.*}
-if [[ -f "$exe_name" && "$exe_name" -nt "$filename" ]];then
-    echo "Skipping compilation, file already compiled"
-else
-    gcc -o $exe_name $filename -Wall -Wextra -lm
-fi
+compile() {
+    filename="$1"
+    exe_name="exe_${filename%.*}"
+    if [[ -f "$exe_name" && "$exe_name" -nt "$filename" ]];then
+        echo "Skipping compilation, file already compiled"
+    else
+        echo "Compiling $filename..."
+        if ! gcc -o $exe_name $filename -Wall -Wextra -lm; then
+            exit 1
+        fi
+    fi
+}
 
-./$exe_name
-rm $exe_name
+if [[ "$1" == "-c" || "$1" == "--clean" ]]; then
+    echo "Cleaning..."
+    rm exe_*
+elif [[ "$1" == "-o" || "$1" == "--only" ]]; then
+    compile "$2"
+else
+    compile "$1"
+
+    ./$exe_name
+    rm $exe_name
+fi
 
 echo ""
 echo "=========================="
