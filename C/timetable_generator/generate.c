fa6f0ad8ef9f7e0f334ce6161624ab529befa8dd
YerdosNar <besway2000@gmail.com>
2026-03-04 15:19:04 +0900

    test input

--- C/timetable_generator/generate.c
+++ C/timetable_generator/generate.c
@@ -76,6 +76,47 @@ void print_timetable(timetable_t t) {
     printf("+========================================================================================+\n");
 }
 
+void save_timetable(timetable_t t, FILE *f) {
+    char line[] = "+========================================================================================+\n";
+    fwrite(line, strlen(line), 1, f);
+    char buff[512];
+    snprintf(buff, 511, "|    Time     |");
+    fwrite(buff, strlen(buff), 1, f);
+    for (int d = 0; d < 5; d++) {
+        snprintf(buff, 511, "     %-5s    |", t.day[d].day_name);
+        fwrite(buff, strlen(buff), 1, f);
+    }
+    fwrite("\n", 1, 1, f);
+    fwrite(line, strlen(line), 1, f);
+
+    for (int h = 0; h < 9; h++) {
+        snprintf(buff, 511, "| %02d:00-%02d:00 |", h + 9, h + 10);
+        fwrite(buff, strlen(buff), 1, f);
+
+        for (int d = 0; d < 5; d++) {
+            int count = t.day[d].hour[h].assigned_count;
+
+            if (count == 0) {
+                snprintf(buff, 511, " %-12s |", "--");
+                fwrite(buff, strlen(buff), 1, f);
+            } else if (count == 1) {
+                snprintf(buff, 511, " %-12.12s |", t.day[d].hour[h].worker_names[0]);
+                fwrite(buff, strlen(buff), 1, f);
+            } else if (count == 2) {
+                // Shorten names to fit visually if there are 2 people
+                snprintf(buff, 511, " %-4.4s & %-5.5s |", t.day[d].hour[h].worker_names[0], t.day[d].hour[h].worker_names[1]);
+                fwrite(buff, strlen(buff), 1, f);
+            } else {
+                snprintf(buff, 511, " %d workers    |", count);
+                fwrite(buff, strlen(buff), 1, f);
+            }
+        }
+        fwrite("\n", 1, 1, f);
+    }
+    snprintf(buff, 511, "+========================================================================================+\n");
+    fwrite(buff, strlen(buff), 1, f);
+}
+
 int main() {
     Person group[MAX_PEOPLE];
     int num_people;
@@ -95,6 +136,9 @@ int main() {
 
     timetable_t final_schedule = schedule(group, num_people);
     print_timetable(final_schedule);
+
+    FILE *fp = fopen("final_schedule.txt", "w");
+    save_timetable(final_schedule, fp);
 
     return 0;
 }
