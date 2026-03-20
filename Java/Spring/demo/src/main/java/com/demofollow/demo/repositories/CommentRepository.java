6cc932c86293a97f255d6d9dd0e81f15ca66db6e
YerdosNar <besway2000@gmail.com>
2026-03-20 23:37:45 +0900

    day4

--- Java/Spring/demo/src/main/java/com/demofollow/demo/repositories/CommentRepository.java
+++ Java/Spring/demo/src/main/java/com/demofollow/demo/repositories/CommentRepository.java
@@ -1,10 +1,13 @@
 package com.demofollow.demo.repositories;
 
+import org.springframework.stereotype.Repository;
+
 import com.demofollow.demo.models.Comment;
 
 /**
  * CommentRepository
  */
+@Repository
 public interface CommentRepository {
 
     void storeComment(Comment comment);
