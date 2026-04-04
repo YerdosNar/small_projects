569e4966a51d30add7ca44775141738b64f3e317
YerdosNar <besway2000@gmail.com>
2026-04-05 07:00:38 +0900

    finish

--- Python/FastAPI/week4/main.py
+++ Python/FastAPI/week4/main.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import FastAPI, Path
+from fastapi import Body, FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -10,15 +10,24 @@ class Item(BaseModel):
     price: float
     tax: float | None = None
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
 @app.put("/items/{item_id}")
 async def update_item(
-        item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
-        q: str | None = None,
-        item: Item | None = None
+        item_id: int,
+        item: Item,
+        user: User,
+        importance: Annotated[int, Body(qt=0)],
+        q: str | None = None
 ):
-    results = {"item_id": item_id}
+    results = {
+        "item_id": item_id,
+        "item": item,
+        "user": user,
+        "importance": importance
+    }
     if q:
         results.update({"q": q})
-    if item:
-        results.update({"item": item})
     return results
