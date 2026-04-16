9c813af9e0f5f77bc5b90a99b287f6abe755dc76
YerdosNar <besway2000@gmail.com>
2026-04-16 20:49:43 +0900

    rewriting

--- C/open_p2p/src/peer.c
+++ C/open_p2p/src/peer.c
@@ -1,16 +1,47 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-#include "../include/crypto.h"
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 8888
+
+int connect_to_server(const char *ip, uint16_t port) {
+	int fd;
+	struct sockaddr_in sa = {0};
+
+	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	sa.sin_family = AF_INET;
+	sa.sin_port   = htons(port);
+
+	if (inet_pton(AF_INET, ip, &sa.sin_addr) <= 0) {
+		perror("inet_pton");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	if (connect(fd, (struct sockaddr *)&sa, sizeof(sa)) < 0) {
+		perror("connect");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	return fd;
+}
 
 int main() {
-	int sockfd, connfd;
-	struct sockaddr_in sa, ca;
-
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		fprintf(stderr, "ERROR: main->socket()\n");
-		return 1;
-	}
-	printf("Peer.\n");
-
+	char *server_ip = SERVER_IP;
+	uint16_t port = SERVER_PORT;
+	int fd = connect_to_server(server_ip, port);
+	printf("Peer: connected to %s:%d (fd=%d)\n",
+			server_ip, port, fd);
+	close(fd);
 	return 0;
 }
--- C/open_p2p/src/rendezvous.c
+++ C/open_p2p/src/rendezvous.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-int main() {
-	printf("Rendezvous.\n");
+#define PORT 8888
+#define BACKLOG 8
 
+int setup_listen_fd(uint16_t port) {
+	int l_fd;
+	struct sockaddr_in sa = {0};
+
+	if((l_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+
+	sa.sin_family 		= AF_INET;
+	sa.sin_addr.s_addr 	= htonl(INADDR_ANY);
+	sa.sin_port 		= htons(port);
+
+	if (bind(l_fd, (struct sockaddr *)&sa , sizeof(sa)) < 0) {
+		perror("bind");
+		close(l_fd);
+		exit(EXIT_FAILURE);
+	}
+	return l_fd;
+}
+
+int main(void) {
+	uint16_t port = PORT;
+	int l_fd = setup_listen_fd(port);
+	if (listen(l_fd, BACKLOG) < 0) {
+		perror("listen");
+		close(l_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Rendezvous: listening on port: %d\n", port);
+
+	struct sockaddr_in ca = {0};
+	socklen_t client_len = sizeof(ca);
+	char client_ip[INET_ADDRSTRLEN];
+	int client_fd = accept(l_fd, (struct sockaddr *)&ca, &client_len);
+	if (client_fd < 0) {
+		perror("accept");
+		close(l_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	if (inet_ntop(AF_INET, &ca.sin_addr, client_ip, sizeof(client_ip)) == NULL) {
+		perror("inet_ntop");
+	} else {
+		printf("Rendezvous: accepted connection from %s:%d (fd=%d)\n",
+				client_ip, ntohs(ca.sin_port), client_fd);
+	}
+
+	close(l_fd);
 	return 0;
 }
